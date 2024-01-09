79c52d71cd39bfd148c8a48070b32df5df84327d
j6a6pedr6 <151445247+j6a6pedr6@users.noreply.github.com>
2024-01-09 09:43:23 -0300

    novos exercícios

---
+++ aula011-condicoes/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log('Ditija com cuidado.')
+if (vel > 60) {
+    console.log(`Você exedeu o limite de velocidade, SERÁ MULTADO!`)
+}
+console.log('Sempre use o cinto de seguraça')
---
+++ aula011-condicoes/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'França'
+console.log(`Nasceu em ${pais}`)
+if (pais != 'Brasil') {
+    console.log('Você é estrangeiro.')
+} else {
+    console.log('Você é brasileiro.')
+}
