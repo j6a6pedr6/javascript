b060bfbc9f5f7e9aafd4572023f32f7fc7cf3bbb
j6a6pedr6 <151445247+j6a6pedr6@users.noreply.github.com>
2024-01-10 13:36:21 -0300

    aula0013

---
+++ aula013-repeticoes/ex015.js
@@ -0,0 +1,21 @@
+var c = 1
+while (c <= 6) {
+    console.log('Oi, tudo bem!')
+    c++
+}
+
+var y = 1
+do {
+    console.log('Não!')
+    y++
+} while (y <= 6)
+
+
+/*
+console.log('Oi, tudo bem!')
+console.log('Oi, tudo bem!')
+console.log('Oi, tudo bem!')
+console.log('Oi, tudo bem!')
+console.log('Oi, tudo bem!')
+console.log('Oi, tudo bem!')
+*/
---
+++ aula013-repeticoes/node_f57fefe86bd31.tmp.js
@@ -0,0 +1,21 @@
+var c = 1
+while (c <= 6) {
+    console.log('Oi, tudo bem!')
+    c++
+}
+
+var y = 1
+do {
+    console.log('Não!')
+    
+} while (y <= 6)
+
+
+/*
+console.log('Oi, tudo bem!')
+console.log('Oi, tudo bem!')
+console.log('Oi, tudo bem!')
+console.log('Oi, tudo bem!')
+console.log('Oi, tudo bem!')
+console.log('Oi, tudo bem!')
+*/
