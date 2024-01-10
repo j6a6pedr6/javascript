a09789045625097684b45198efb2e40ca42926d4
j6a6pedr6 <151445247+j6a6pedr6@users.noreply.github.com>
2024-01-10 14:07:07 -0300

    aula014

--- aula013-repeticoes/ex015.js
+++ aula013-repeticoes/ex015.js
@@ -1,15 +1,19 @@
+var y = 1
+do {
+    console.log('Oi, tudo bem!')
+    console.log('Não!')
+    y++
+} while (y <= 6)
+
+/*
 var c = 1
 while (c <= 6) {
     console.log('Oi, tudo bem!')
     c++
 }
+*/
 
-var y = 1
-do {
-    console.log('Não!')
-    y++
-} while (y <= 6)
-
+//--------------------------------
 
 /*
 console.log('Oi, tudo bem!')
